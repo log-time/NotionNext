e4e65befad119ff7b38a570c6d3f058d556a9783
logtime <66826612+log-time@users.noreply.github.com>
2024-12-27 15:34:37 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '日常记录', url: '/category/日常记录' },
   HEO_HERO_CATEGORY_2: { title: '实用教程', url: '/category/实用教程' },
-  HEO_HERO_CATEGORY_3: { title: '学习提升', url: '/category/学习提升' },
+  HEO_HERO_CATEGORY_3: { title: '学习参考', url: '/category/学习参考' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
